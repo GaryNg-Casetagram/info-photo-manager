df581b5eefcedfcd11ff635d850071165f098d3c
GaryNg_ctf <gary.ng@casetagram.com>
2025-09-18 18:21:27 +0800

    Fix server configuration for Vue.js SPA - Remove static file serving and add CORS for development

--- server.js
+++ server.js
@@ -86,12 +86,18 @@ db.serialize(() => {
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
 app.use('/uploads', express.static('uploads'));
 
-// Routes
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// CORS middleware for development
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
 });
 
 // Get all entries with files
